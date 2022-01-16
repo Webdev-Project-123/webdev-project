8ae9b88864d988a8c16c121b12baf8a265955d47
DickyDicky7 <87304213+DickyDicky7@users.noreply.github.com>
2022-01-16 17:12:06 +0700

    add eslint

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const morgan = require('morgan');
+
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(morgan('common'));
 
 app.get('/', (req, res) => {
   res.send('Yo');
