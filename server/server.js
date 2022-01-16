a6eabfe3adc55e0b9a08739da6ace5fb7037faea
DickyDicky7 <87304213+DickyDicky7@users.noreply.github.com>
2022-01-16 12:39:25 +0700

    nothing special

--- server/server.js
+++ server/server.js
@@ -4,3 +4,9 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get('/', (req, res) => {
+  res.send('Yo');
+});
+
+const PORT = 3000;
+app.listen(PORT, console.log(`Express on ${PORT}`));
