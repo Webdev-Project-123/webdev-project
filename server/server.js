73b5dff7ce144f1b5ab96c8cdf06eb636cb6f0c1
DickyDicky7 <87304213+DickyDicky7@users.noreply.github.com>
2022-01-20 00:42:28 +0700

    connect server

--- server/server.js
+++ server/server.js
@@ -22,19 +22,24 @@ app.use(cors());
 // * Import path module
 const path = require('path');
 
-// * Serving static files in public folder
-app.use(express.static(path.join(__dirname, 'public')));
+// // * Serving static files in public folder
+// app.use(express.static(path.join(__dirname, 'public')));
 
-// // * Import ramda module
-// const R = require('ramda');
+// * Serving static files in public folder
+app.use(
+  express.static(path.join(__dirname, '../client/build')),
+);
+
+// * Import ramda module
+const R = require('ramda');
 
 // // * Import Sanctuary module
 // const S = require('sanctuary');
 
 // * Load the server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
 
 // // * Import http-errors module
@@ -42,7 +47,19 @@ app.listen(PORT, () => {
 
 // * Get homepage
 app.get('/', (req, res) => {
-  res.status(200).redirect('/homepage/index.html');
+  res.sendFile(
+    path.join(__dirname, '../client/build/index.html'),
+    (err) => {
+      R.when(
+        err,
+        res
+          .status(404)
+          .sendFile(
+            path.join(__dirname, './public/404.html'),
+          ),
+      );
+    },
+  );
 });
 
 // * Import api module
@@ -50,10 +67,3 @@ const api = require('./api');
 
 // * Routing
 app.use('/api', api);
-
-// * Error handling
-app.use((req, res) => {
-  res
-    .status(404)
-    .sendFile(path.join(__dirname, 'public/404.html'));
-});
