2732d452dbbe44e2386280c35134b6c4737305d3
AkiraNoob <narutoducphuong76@gmail.com>
2022-01-21 00:24:33 +0700

    add font, custom tailwind

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,16 @@
 module.exports = {
-  content: ['./index.html', './src/**/*.jsx'],
-  darkMode: 'class', // 'media' or 'class'
+  content: ["./index.html", "./src/**/*.jsx"],
+  darkMode: "class", // 'media' or 'class'
   theme: {
     extend: {
+      boxShadow: {
+        phuongCustom: "3px 3px 5px 0px rgba(0, 0, 0, 0.2)",
+        phuongCustomActive: "1px 1px 2px 0px rgba(0, 0, 0, 0.2)",
+      },
       fontFamily: {
-        sans: ['Open Sans', 'sans-serif'],
+        sans: ["Open Sans", "sans-serif"],
+        sourceCodePro: ["Source Code Pro", "sans-serif"],
+        ubuntu: ["Ubuntu", "sans-serif"],
       },
     },
   },
