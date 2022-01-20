06c22dc4bd7a7272e91118f34b9b996234a053ed
AkiraNoob <narutoducphuong76@gmail.com>
2022-01-20 18:47:47 +0700

    config axiosClient

---
+++ client/src/apiClient/axiosClient.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+import queryString from "query-string";
+
+const axiosClient = axios.create({
+  baseURL: "",
+  headers: {
+    // "current-type": "application/json-patch+json",
+  },
+  paramsSerializer: (param) => queryString.stringify(param),
+});
+
+axiosClient.interceptors.request.use(
+  function (config) {
+    const accessToken = window.localStorage.getItem("accessToken");
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+
+axiosClient.interceptors.response.use(
+  function (res) {
+    if (res && res.data) {
+      return res.data;
+    } else
+      return {
+        res,
+      };
+  },
+  function (err) {
+    return Promise.reject(err);
+  }
+);
+
+export default axiosClient;
