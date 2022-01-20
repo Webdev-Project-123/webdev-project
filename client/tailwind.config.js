68c3c5592618f92a4883805e3ae1f2f3562aef63
Tuấn Hoàng Đình Anh <20522110@ms.uit.edu.vn>
2022-01-20 22:03:01 +0700

    update tailwind configuration file

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,12 @@
 module.exports = {
-  content: [ './index.html', './src/**/*.jsx' ],
+  content: ['./index.html', './src/**/*.jsx'],
   darkMode: 'class', // 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Open Sans', 'sans-serif'],
+      },
+    },
   },
   variants: {
     extend: {},
