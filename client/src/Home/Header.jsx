feb62f23695a983360cb9741fb35b1dada14af9e
Tuấn Hoàng Đình Anh <20522110@ms.uit.edu.vn>
2022-01-20 23:52:49 +0700

    update src/Home/Header.jsx

--- client/src/Home/Header.jsx
+++ client/src/Home/Header.jsx
@@ -9,16 +9,16 @@ const loginIcon = <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 rot
 </svg>
 
 const Header = ({ isLogin }) => {
-  return <div className="px-1 sm:px-9 font-sans flex items-center bg-[#FF6701] h-14 font-bold text-white space-x-2">
+  return <div className="px-1 sm:px-9 font-sans flex items-center justify-between bg-[#FF6701] h-14 font-bold text-white space-x-2">
 
-    <h1 className="text-2xl sm:text-3xl flex items-center w-1/3">
+    <h1 className="text-2xl sm:text-3xl flex items-center">
       <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 mr-2" fill="white" viewBox="0 0 24 24" stroke="currentColor">
         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1} d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
       </svg>
       WORDY.
     </h1>
 
-    <div className='border rounded-md border-whites w-1/3 py-1 px-4 flex items-center'>
+    <div className='border rounded-md border-whites sm:w-1/2 md:w-[42%] lg:w-1/3 py-1 px-4 flex items-center'>
       <input
         className="border-0 text-sm text-white bg-inherit placeholder:text-white w-full outline-0"
         placeholder="Tìm sách..."
@@ -28,7 +28,7 @@ const Header = ({ isLogin }) => {
       </svg>
     </div>
 
-    <nav className="flex w-1/3 justify-end">
+    <nav>
       <div className="flex items-center gap-3 sm:gap-8">
         <svg className="w-[22px] h-[22px]" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round"
           strokeWidth={1.5} d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
