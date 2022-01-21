74fed22b42ba490e0ff67ba83bd0261884963ca8
DickyDicky7 <87304213+DickyDicky7@users.noreply.github.com>
2022-01-22 01:06:40 +0700

    fixing something

--- server/api/products/controller.js
+++ server/api/products/controller.js
@@ -7,13 +7,13 @@ module.exports = {
     const DTO = await productsService.hot();
 
     if (R.equals(DTO.status, 410)) next(createErr(410, 'Products gone'));
-    else res.status(200).json(DTO);
+    else res.status(304).json(DTO);
   },
 
   get: async (req, res, next) => {
-    const DTO = await productsService.get(req.params.id);
+    const DTO = await productsService.get(req.params.prodId);
 
     if (R.equals(DTO.status, 410)) next(createErr(410, 'Product gone'));
-    else res.status(200).json(DTO);
+    else res.status(304).json(DTO);
   },
 };
