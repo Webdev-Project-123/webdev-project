41c1edbf3c1c55b5e7da7041239cf042c908d726
lemauanhphong <lemauanhphong@gmail.com>
2022-01-21 14:58:20 +0700

    add jwt mechaism

--- server/api/index.js
+++ server/api/index.js
@@ -8,10 +8,10 @@ const products = require('./products');
 const categories = require('./categories');
 
 router.use('/auth', auth);
-router.use('/cart', cart);
-router.use('/bought', bought);
-router.use('/upload', upload);
-router.use('/products', products);
-router.use('/categories', categories);
+// router.use('/cart', cart);
+// router.use('/bought', bought);
+// router.use('/upload', upload);
+// router.use('/products', products);
+// router.use('/categories', categories);
 
 module.exports = router;
