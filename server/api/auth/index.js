fc4cd5927b69c359ef6db203ef506127a7f74dac
lemauanhphong <lemauanhphong@gmail.com>
2022-01-21 22:25:37 +0700

    Change method for /api/auth/token

--- server/api/auth/index.js
+++ server/api/auth/index.js
@@ -4,6 +4,6 @@ const authController = require('./controller');
 
 router.post('/sign-up', authController.signup);
 router.post('/login', authController.login);
-router.delete('/token', authController.token);
+router.patch('/token', authController.token);
 
 module.exports = router;
