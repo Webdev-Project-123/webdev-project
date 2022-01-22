70b1cf75dcb02d688922fde69c10d761dbff00a3
Tuấn Hoàng Đình Anh <20522110@ms.uit.edu.vn>
2022-01-22 18:29:46 +0700

    update ProductPage (all products)

--- client/src/ProductPage/Filter.jsx
+++ client/src/ProductPage/Filter.jsx
@@ -8,19 +8,21 @@ const categories = ['Romance', 'Fantasy', 'Horror', 'Manga', 'Sci-fi', 'Mystery'
 
 const Filter = () => {
 
-  return <div className='ml-16 w-[240px] cursor-pointer bg-white rounded-lg h-fit'>
+  return <div className='ml-16 w-[280px] cursor-pointer bg-white rounded-lg h-fit'>
     <div className='py-2 font-extrabold text-center text-xl bg-[#FEA82F] rounded-t-lg'>CATEGORY</div>
 
-    {categories.map((category, index) => {
-      if (index === categories.length - 1)
-        return <div key={index} className='filter rounded-b-lg'>
+    <nav>
+      {categories.map((category, index) => {
+        if (index === categories.length - 1)
+          return <ul key={index} className='filter rounded-b-lg'>
+            {category}
+          </ul>
+        return <ul key={index} className='filter'>
           {category}
-        </div>
-      return <div key={index} className='filter'>
-        {category}
-      </div>
-    }
-    )}
+        </ul>
+      }
+      )}
+    </nav>
   </div>;
 };
 
--- client/src/ProductPage/ProductPage.jsx
+++ client/src/ProductPage/ProductPage.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Footer from '../Home/Footer';
 import Header from '../Home/Header';
 import Filter from './Filter';
+import PaginationBar from './PaginationBar';
 import PaginationPage from './PaginationPage';
 import Spinner from './Spinner';
 
@@ -65,6 +66,46 @@ const productData = [
   {
     name: 'IDK',
     image: 'https://preview.redd.it/c6rlndlo41851.jpg?width=640&crop=smart&auto=webp&s=f28a8e163c0670c81a77b1c07e498d6b14a60e4d'
+  },
+  {
+    name: 'IDK',
+    image: 'https://preview.redd.it/c6rlndlo41851.jpg?width=640&crop=smart&auto=webp&s=f28a8e163c0670c81a77b1c07e498d6b14a60e4d'
+  },
+  {
+    name: 'IDK',
+    image: 'https://preview.redd.it/c6rlndlo41851.jpg?width=640&crop=smart&auto=webp&s=f28a8e163c0670c81a77b1c07e498d6b14a60e4d'
+  },
+  {
+    name: 'IDK',
+    image: 'https://preview.redd.it/c6rlndlo41851.jpg?width=640&crop=smart&auto=webp&s=f28a8e163c0670c81a77b1c07e498d6b14a60e4d'
+  },
+  {
+    name: 'IDK',
+    image: 'https://preview.redd.it/c6rlndlo41851.jpg?width=640&crop=smart&auto=webp&s=f28a8e163c0670c81a77b1c07e498d6b14a60e4d'
+  },
+  {
+    name: 'IDK',
+    image: 'https://preview.redd.it/c6rlndlo41851.jpg?width=640&crop=smart&auto=webp&s=f28a8e163c0670c81a77b1c07e498d6b14a60e4d'
+  },
+  {
+    name: 'IDK',
+    image: 'https://preview.redd.it/c6rlndlo41851.jpg?width=640&crop=smart&auto=webp&s=f28a8e163c0670c81a77b1c07e498d6b14a60e4d'
+  },
+  {
+    name: 'IDK',
+    image: 'https://preview.redd.it/c6rlndlo41851.jpg?width=640&crop=smart&auto=webp&s=f28a8e163c0670c81a77b1c07e498d6b14a60e4d'
+  },
+  {
+    name: 'IDK',
+    image: 'https://preview.redd.it/c6rlndlo41851.jpg?width=640&crop=smart&auto=webp&s=f28a8e163c0670c81a77b1c07e498d6b14a60e4d'
+  },
+  {
+    name: 'IDK',
+    image: 'https://preview.redd.it/c6rlndlo41851.jpg?width=640&crop=smart&auto=webp&s=f28a8e163c0670c81a77b1c07e498d6b14a60e4d'
+  },
+  {
+    name: 'IDK',
+    image: 'https://preview.redd.it/c6rlndlo41851.jpg?width=640&crop=smart&auto=webp&s=f28a8e163c0670c81a77b1c07e498d6b14a60e4d'
   }
 ]
 
@@ -72,22 +113,32 @@ const ProductPage = () => {
   const [products, setProducts] = useState(productData);
   const [loading, setLoading] = useState(false);
   const [currPage, setCurrPage] = useState(1);
-  const [productsPerPage, setProductsPerPage] = useState(12);
+  const [productsPerPage, setProductsPerPage] = useState(4);
 
   const currProducts = products.slice((currPage - 1) * productsPerPage, currPage * productsPerPage);
   console.log(currProducts.length);
+
+  const paginate = (pageNumber) => setCurrPage(pageNumber);
+
 
   return <div className='bg-[#FCECDD]'>
     <Header isLogin={false} />
 
     <div className='my-8 flex gap-4'>
-      {/* Filter Bar */}
+      {/* Filter Bar | Left Content */}
       <Filter className='inline' />
 
-      {/* Product List */}
-      <div className='w-full bg-white rounded-lg mr-16 flex items-center justify-center'>
-        <PaginationPage products={currProducts} loading={loading} />
+      {/* Right Content */}
+      <div className='mr-16 w-full'>
+        {/* Product List */}
+        <div className='w-full bg-white rounded-lg'>
+          <PaginationPage products={currProducts} loading={loading} />
+        </div>
+
+        {/* Pagination Bar */}
+        <PaginationBar productsPerPage={productsPerPage} numOfProducts={products.length} paginate={paginate} currPage={currPage} />
       </div>
+
     </div>
     <Footer />
   </div>;
