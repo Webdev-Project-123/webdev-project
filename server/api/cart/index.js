2a07ac4b0c102bf282748f04d4d4507fd29519f4
dongkimcmqt3902 <20521339@gm.uit.edu.vn>
2022-01-23 01:22:15 +0700

    add cart

--- server/api/cart/index.js
+++ server/api/cart/index.js
@@ -4,6 +4,8 @@ const cartController = require("./controller");
 
 const authorize = require("../../common/authorization");
 
-router.get("/:userId", authorize.isValid, cartController.cartInfo);
+router.get("/:userId", [authorize.isValid, authorize.checkPermission], cartController.cartInfo);
+router.put("/:userId/add", [authorize.isValid, authorize.checkPermission], cartController.cartAdd);
+router.get("/:userId/delete", [authorize.isValid, authorize.checkPermission], cartController.cartDelete);
 
 module.exports = router;
