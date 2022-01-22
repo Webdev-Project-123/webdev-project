f56100f6c9ff9e06bd54b271816ec4c2ceb662ba
AkiraNoob <narutoducphuong76@gmail.com>
2022-01-22 19:31:13 +0700

    update Profile

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,10 +2,16 @@ import React from "react";
 import SignUp from "./Person/SignUp";
 import Login from "./Person/Login";
 import Profile from "./Person/Profile";
+import Home from "./Home/Home";
+import Header from "./Home/Header";
+import Footer from "./Home/Footer";
+import Spinner from "./ProductPage/Spinner";
 function App() {
   return (
     <div>
-      <Home />
+      {/* <Header /> */}
+      <Profile />
+      {/* <Footer /> */}
     </div>
   );
 }
