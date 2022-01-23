fc88935dd5d85afb76805dfa9817e620ad33beac
AkiraNoob <narutoducphuong76@gmail.com>
2022-01-23 13:38:23 +0700

    fix addProducts

--- client/src/Person/Admin/AddProduct.jsx
+++ client/src/Person/Admin/AddProduct.jsx
@@ -4,6 +4,30 @@ import Input from "../components/Input";
 import Label from "../components/Label";
 
 function AddProduct() {
+  const [input, setInput] = useState({
+    name: "",
+    image: "",
+    author: [""],
+    categories: [],
+    uploadDate: "",
+    publishComp: "",
+    publishDate: "",
+    price: "",
+    salePrice: "",
+    pages: "",
+    language: "",
+    desc: "",
+    inStock: "",
+  });
+
+  const [date, setDate] = useState({
+    date: "",
+    month: "",
+    year: "",
+  });
+
+  const [avatar, setAvatar] = useState();
+
   const inputStyle =
     "peer w-9/10 flex-shirnk-0 flex-grow mb-4 py-2 pr-2 outline-none border-b-2 border-b-[#47392b] placeholder:text-[#47392b] bg-transparent placeholder:text-[#51050F] placeholder:text-[1rem] placeholder:font-robotoS focus:placeholder:text-transparent placeholder:transition-colors placeholder:ease-out";
   const labelStyle =
@@ -14,7 +38,17 @@ function AddProduct() {
   const labelDateStyle =
     "w-[30%] flex justify-center items-center flex-wrap gap-3 text-[#47392b]";
 
-  const [avatar, setAvatar] = useState();
+  const handleInputDate = (e) => {
+    const newDate = { ...date };
+    newDate[e.target.name] = e.target.value;
+    setDate(newDate);
+  };
+
+  const handleInputBookInfo = (e) => {
+    const newInput = { ...input };
+    newInput[e.target.name] = e.target.menu;
+    setInput(newInput);
+  };
 
   const handleUpload = () => {
     document.querySelector("#file").click();
@@ -45,7 +79,41 @@ function AddProduct() {
   return (
     <div className="w-screen min-h-screen md:px-10 px-3 flex font-roboto py-10">
       <div className="w-full h-auto flex flex-col lg:flex-row justify-center items-center rounded-md">
-        <div className="lg:w-[55%] w-full lg:h-full h-auto bg-[#FCECDD] flex flex-col justify-center items-center pt-[3rem] lg:pb-0 pb-[2rem] gap-8 rounded-l-md">
+        <div className=" relative lg:w-[55%] w-full lg:h-full h-auto bg-[#FCECDD] flex flex-col justify-center items-center pt-[3rem] lg:pb-0 pb-[2rem] gap-8 rounded-l-md">
+          <div className="absolute top-0 left-0 w-[6%] h-[10%]">
+            <button className="w-full h-1/2 flex justify-center items-center bg-[#ff1818]">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6 text-[#fcecdd]"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+            <button className="w-full h-1/2 flex justify-center items-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M4 6h16M4 10h16M4 14h16M4 18h16"
+                />
+              </svg>
+            </button>
+          </div>
           <h1 className="w-full h-auto flex justify-center items-center text-2xl font-robotoS text-[#58493a]">
             <span className="before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-[#f5b14a] relative">
               <span className="relative text-[#ffeedf]">
@@ -60,6 +128,8 @@ function AddProduct() {
                 type="text"
                 name="name"
                 placeholder={"Book's name"}
+                value={input.name}
+                onChange={handleInputBookInfo}
               />
             </Label>
             <Label className={labelStyle}>
@@ -68,6 +138,8 @@ function AddProduct() {
                 type="text"
                 name="author"
                 placeholder={"Book's author"}
+                value={input.author}
+                onChange={handleInputBookInfo}
               />
             </Label>
             <Label className={labelStyle}>
@@ -75,7 +147,9 @@ function AddProduct() {
                 className={inputStyle}
                 type="text"
                 name="categories"
+                value={input.categories}
                 placeholder={"Categories, separate by spacing"}
+                onChange={handleInputBookInfo}
               />
             </Label>
             <Label className={labelStyle}>
@@ -84,6 +158,8 @@ function AddProduct() {
                 type="text"
                 name="publishCom"
                 placeholder={"Publish company"}
+                value={input.publishComp}
+                onChange={handleInputBookInfo}
               />
             </Label>
             <div className="flex justify-between flex-wrap items-center w-full h-auto">
@@ -95,7 +171,9 @@ function AddProduct() {
                   min={1}
                   max={31}
                   placeholder={"Date"}
+                  value={date.date}
                   className={inputDateStyle}
+                  onChange={handleInputDate}
                 />
               </Label>
               <Label className={labelDateStyle}>
@@ -105,7 +183,9 @@ function AddProduct() {
                   min={1}
                   max={12}
                   placeholder={"Month"}
+                  value={date.month}
                   className={inputDateStyle}
+                  onChange={handleInputDate}
                 />
               </Label>
               <Label className={labelDateStyle}>
@@ -114,7 +194,9 @@ function AddProduct() {
                   name={"year"}
                   min={1}
                   placeholder={"Year"}
+                  value={date.year}
                   className={inputDateStyle}
+                  onChange={handleInputDate}
                 />
               </Label>
             </div>
@@ -125,6 +207,8 @@ function AddProduct() {
                   type="number"
                   name="price"
                   placeholder={"Price"}
+                  value={input.price}
+                  onChange={handleInputBookInfo}
                 />
               </Label>
               <Label className={labelDateStyle}>
@@ -133,6 +217,8 @@ function AddProduct() {
                   type="number"
                   name="salePrice"
                   placeholder={"Sale"}
+                  value={input.salePrice}
+                  onChange={handleInputBookInfo}
                 />
               </Label>
               <Label className={labelDateStyle}>
@@ -141,6 +227,8 @@ function AddProduct() {
                   type="number"
                   name="pages"
                   placeholder={"Pages"}
+                  value={input.pages}
+                  onChange={handleInputBookInfo}
                 />
               </Label>
             </div>
@@ -148,8 +236,10 @@ function AddProduct() {
               <Input
                 className={inputStyle}
                 type="text"
-                name="lang"
+                name="language"
                 placeholder={"Language of the book"}
+                value={input.language}
+                onChange={handleInputBookInfo}
               />
             </Label>
             <Label className={labelStyle}>
@@ -157,6 +247,8 @@ function AddProduct() {
                 className={inputStyle}
                 type="text"
                 name="desc"
+                value={input.desc}
+                onChange={handleInputBookInfo}
                 placeholder={"Description"}
               />
             </Label>
@@ -165,7 +257,9 @@ function AddProduct() {
                 className={inputStyle}
                 type="number"
                 name="inStock"
+                value={input.inStock}
                 placeholder={"Remaining amount of books"}
+                onChange={handleInputBookInfo}
               />
             </Label>
           </div>
@@ -197,7 +291,7 @@ function AddProduct() {
           >
             <img
               src={avatar?.preview}
-              className="w-full h-full object-contain rounded-md relative before:absolute before:rounded-md before:w-full before:h-full before:top-0 before:left-0 before:bg-[#FCECDD]"
+              className="w-full h-full object-contain rounded-md block relative before:absolute before:rounded-md before:w-full before:h-full before:top-0 before:left-0 before:bg-[#FCECDD]"
               alt=""
             />
           </div>
