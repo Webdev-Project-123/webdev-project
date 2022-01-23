a90283cae781b35e6c76a8d34eb200b05ee89f80
dongkimcmqt3902 <20521339@gm.uit.edu.vn>
2022-01-23 13:47:54 +0700

    get -> delete (cartDelete)

--- server/api/cart/index.js
+++ server/api/cart/index.js
@@ -6,6 +6,6 @@ const authorize = require("../../common/authorization");
 
 router.get("/:userId", authorize.checkPermission, cartController.cartInfo);
 router.put("/:userId/add", authorize.checkPermission, cartController.cartAdd);
-router.get("/:userId/delete", authorize.checkPermission, cartController.cartDelete);
+router.delete("/:userId/delete", authorize.checkPermission, cartController.cartDelete);
 
 module.exports = router;
