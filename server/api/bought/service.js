9f9e08b909e31a55c812b8ded05b43da990e380e
lemauanhphong <lemauanhphong@gmail.com>
2022-01-23 08:59:18 +0700

    Add try catch

--- server/api/bought/service.js
+++ server/api/bought/service.js
@@ -2,12 +2,19 @@ const db = require('../../models/db');
 
 module.exports = {
   boughtInfo: async (req, res, next) => {
-    const list = db.get('users').find({ id: req.userid }).value();
-    return {
-      error: false,
-      statusCode: 200,
-      msg: 'OK',
-      list: list.bought,
-    };
+    try {
+      const list = db.get('users').find({ id: req.userid }).value();
+      return {
+        error: false,
+        statusCode: 200,
+        msg: 'OK',
+        list: list.bought,
+      };
+    } catch {
+      return {
+        statusCode: 500,
+        msg: 'Error in boughtInfo',
+      };
+    }
   },
 };
