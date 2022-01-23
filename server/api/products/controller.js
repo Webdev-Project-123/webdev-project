5e14bae8e31f8ff150698978492019d932245df6
DickyDicky7 <87304213+DickyDicky7@users.noreply.github.com>
2022-01-23 14:02:35 +0700

    upload api done

--- server/api/products/controller.js
+++ server/api/products/controller.js
@@ -4,20 +4,38 @@ const productsService = require('./service');
 
 module.exports = {
   hot: async (req, res, next) => {
-    const DTO = await productsService.hot();
+    try {
+      const DTO = await productsService.hot();
 
-    if (R.equals(DTO.status, 400)) {
-      next(createErr(400, 'BAD REQUEST'));
-    } else res.status(200).json(DTO);
+      if (R.equals(DTO.status, 400)) {
+        next(createErr(400, 'BAD REQUEST'));
+      } else res.status(200).json(DTO);
+    } catch (err) {
+      next(
+        createErr(
+          500,
+          'INTERNAL PRODUCTS HOT METHOD ERROR',
+        ),
+      );
+    }
   },
 
   get: async (req, res, next) => {
-    const DTO = await productsService.get(
-      req.params.prodId,
-    );
+    try {
+      const DTO = await productsService.get(
+        req.params.prodId,
+      );
 
-    if (R.equals(DTO.status, 400)) {
-      next(createErr(400, 'BAD REQUEST'));
-    } else res.status(200).json(DTO);
+      if (R.equals(DTO.status, 400)) {
+        next(createErr(400, 'BAD REQUEST'));
+      } else res.status(200).json(DTO);
+    } catch (err) {
+      next(
+        createErr(
+          500,
+          'INTERNAL PRODUCTS GET METHOD ERROR',
+        ),
+      );
+    }
   },
 };
