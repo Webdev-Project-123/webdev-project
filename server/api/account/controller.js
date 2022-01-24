bdfe2d975d5531c8bb835ce2f4185338e6358ee3
lemauanhphong <lemauanhphong@gmail.com>
2022-01-24 22:19:51 +0700

    add profile

--- server/api/account/controller.js
+++ server/api/account/controller.js
@@ -16,4 +16,20 @@ module.exports = {
       });
     }
   },
+  profile: async (req, res, next) => {
+    try {
+      const DTO = await accountService.profile(req.user.email);
+      const stt = DTO.statusCode ? DTO.statusCode : 500;
+      res.status(stt).json({
+        statusCode: stt,
+        msg: DTO.msg,
+        profile: DTO.profile,
+      });
+    } catch {
+      res.status(500).json({
+        statusCode: 500,
+        msg: 'Error in profile',
+      });
+    }
+  },
 };
